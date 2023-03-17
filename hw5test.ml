52b571c7aba4757b0093326840e8ba4d7a4f00f3
Jaberw0cky <bramseyergordon@clarku.edu>
2023-03-17 11:03:35 -0400

    Update hw5test.ml
    Tests for hw 5
    One test to see if peak first works on a single element q
    One test to add a single element to an empty q

--- hw5test.ml
+++ hw5test.ml
@@ -1,2 +1,8 @@
 let q1 = TwoListDeque.empty;;
 assert(0=(TwoListDeque.size q1));;
+
+let test_q = TwoListDeque.add_first 1 q1
+assert(1 = TwoListDeque.size test_q);;
+
+let test_peak_first = test_q
+assert(1 = TwoListDeque.test_peak_first test_q);;
