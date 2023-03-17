388aa4185ed5bae83e97526466d119373506ae27
Jaberw0cky <125281320+Jaberw0cky@users.noreply.github.com>
2023-03-17 11:11:27 -0400

    Update hw5test.ml

--- hw5test.ml
+++ hw5test.ml
@@ -3,6 +3,6 @@ assert(0=(TwoListDeque.size q1));;
 
 let test_q = TwoListDeque.add_first 1 q1
 assert(1 = TwoListDeque.size test_q);;
-
+(* test 2*)
 let test_peak_first = test_q
 assert(1 = TwoListDeque.test_peak_first test_q);;
