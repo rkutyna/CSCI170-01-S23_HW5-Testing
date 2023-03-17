aeb91fc7a51c966e604f63d7efd82b5502a57f42
dillonr11 <dillonrem@yahoo.com>
2023-03-17 11:06:02 -0400

    Some more test cases

--- hw5test.ml
+++ hw5test.ml
@@ -1,4 +1,7 @@
 let q1 = TwoListDeque.empty;;
 assert(0=(TwoListDeque.size q1));;
-(* add non empty TwoList Deque *)
 assert(TwoListDeque.is_empty q1);;
+(* add non empty TwoList Deque *)
+let qdillon = TwoListDeque.empty;;
+TwoListDeque.add_first 1 qdillon;;
+assert(1=(TwoListDeque.size qdillon));;
