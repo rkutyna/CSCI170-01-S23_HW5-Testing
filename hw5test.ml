2439a90254496ea4448d93ff727c4264b373e8e8
Jaberw0cky <125281320+Jaberw0cky@users.noreply.github.com>
2023-03-17 11:27:40 -0400

    Update hw5test.ml
    Class test to make a 2 element q, then peak, then remove an element

--- hw5test.ml
+++ hw5test.ml
@@ -8,6 +8,7 @@ assert(1 = TwoListDeque.size test_q);;
 let test_peak_first = test_q;;
 assert(1 = TwoListDeque.test_peak_first test_q);;
 
+(* Class test to make a 2 element q, then peak, then remove an element*)
 let class_q = TwoListDeque.add_first "hello" q1;;
 class_q = TwoListDeque.add_first "world" class_q;;
 first_class_q = TwoListDeque.test_peak_first class_q;;
