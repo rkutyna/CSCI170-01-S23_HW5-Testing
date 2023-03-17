efdcb91dc4747aec27723b9fb5af040af7f80ca3
Debrin Adon <93166063+DAdonSuero@users.noreply.github.com>
2023-03-17 11:07:40 -0400

    Update hw5test.ml

--- hw5test.ml
+++ hw5test.ml
@@ -1,2 +1,11 @@
 let q1 = TwoListDeque.empty;;
 assert(0=(TwoListDeque.size q1));;
+
+let test1_0_DA = Deque.is_empty [];;
+assert(test1_0_DA);;
+
+let test2_0_DA = Deque.add_first 1 [2; 3];;
+assert([1; 2; 3] = test2_0_DA);;
+
+let test3_0_DA = Deque.add_last 3 [1; 2];;
+assert([1; 2; 3] = test3_0_DA);;
