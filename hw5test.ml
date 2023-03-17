b958eec18afbff38846a48ecd960dde7f623802f
AbWilson6 <abwilson@clarku.edu>
2023-03-17 11:08:52 -0400

    Update hw5test.ml

--- hw5test.ml
+++ hw5test.ml
@@ -1,2 +1,6 @@
 let q1 = TwoListDeque.empty;;
 assert(0=(TwoListDeque.size q1));;
+
+(* peeking an empty deque returns empty *)
+let q2 = TwoListDeque.empty;;
+assert(Empty=(TwoListDeque.peek q2));;
