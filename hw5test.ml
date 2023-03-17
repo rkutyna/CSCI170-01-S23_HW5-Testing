3910668616be910e6ce1c519a0beba39555cf1c8
liamwangclarku <120049323+liamwangclarku@users.noreply.github.com>
2023-03-17 11:03:45 -0400

    Update hw5test.ml

--- hw5test.ml
+++ hw5test.ml
@@ -1,8 +1,4 @@
 let q1 = TwoListDeque.empty;;
 assert(0=(TwoListDeque.size q1));;
 
-<<<<<<< HEAD
 assert(TwoListDeque.is_empty q1);;
-=======
-assert(TwoListDeque.is_empty q1);;
->>>>>>> 5c734fa013dc2201c1a041a50aadeae5560512b4
