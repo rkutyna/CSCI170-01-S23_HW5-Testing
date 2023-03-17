1953528e96bbf3628acd19c34392f8a772570b34
Levrk <levrolandkalb@gmail.com>
2023-03-17 10:46:41 -0400

    Adding tests for empty/is_empty/add/peek

--- hw5test.ml
+++ hw5test.ml
@@ -1,2 +1,25 @@
 let q1 = TwoListDeque.empty;;
 assert(0=(TwoListDeque.size q1));;
+
+(* ! lev's tests ! *)
+
+(* ? testing empty/is_empty *)
+let test = TwoListDeque.empty
+
+let t1 = assert(TwoListDeque.is_empty test = true)
+
+let test = TwoListDeque.add_first 3 test
+
+let t2 = assert(TwoListDeque.is_empty test = false)
+
+(* ? testing add/peek *)
+let t3 = assert(TwoListDeque.peek_first test = 3)
+
+let t4 = assert(TwoListDeque.peek_last test = 3)
+
+let test = TwoListDeque.add_last 4 test
+
+let t5 = assert(TwoListDeque.peek_first test = 3)
+
+let t6 = assert(TwoListDeque.peek_last test = 4)
+(* ! lev's test end ! *)
