f025556d4bbb2b118b8693673420ee31858f880f
jscudweiss <98598122+jscudweiss@users.noreply.github.com>
2023-03-19 19:07:25 -0400

    Bug fixes
    corrected a number of syntax and implementation understanding errors in the existing tests.
    
    Assuming my version of TwoListDeque works as intended these tests should be correct now.

--- hw5test.ml
+++ hw5test.ml
@@ -1,3 +1,5 @@
+open Deque
+
 (* Dillon's Tests *)
 
 let q1 = TwoListDeque.empty;;
@@ -8,18 +10,18 @@ assert(TwoListDeque.is_empty q1);;
 let qdillon = TwoListDeque.empty;;
 
 (* add 2 ints to Deque *)
-TwoListDeque.add_first 1 qdillon;;
-TwoListDeque.add_last 2 qdillon;;
+let dillon1 = TwoListDeque.add_first 1 qdillon;;
+let dillon2 = TwoListDeque.add_last 2 dillon1;;
 
 (* check size equal to 2 *)
-assert(2=(TwoListDeque.size qdillon));;
+assert(2 = (TwoListDeque.size dillon2));;
 
 (* make sure peeks are working *)
-assert(1=(TwoListDeque.peek_first qdillon));;
-assert(2=(TwoListDeque.peek_last qdillon));;
+assert(1=(TwoListDeque.peek_first dillon2));;
+assert(2=(TwoListDeque.peek_last dillon2));;
 
 (* remove last and check size *)
-TwoListDeque.remove_last qdillon;;
+TwoListDeque.remove_last dillon2;;
 assert(1=(TwoListDeque.size qdillon));;
 
 (* remove first and check size *)
@@ -30,59 +32,63 @@ assert(0=(TwoListDeque.size qdillon));;
 
 
 let ah_hi_bye_check = TwoListDeque.(empty |> add_first "hi" |> add_first "somewhere in between" |> add_first "bye");;
-assert("bye" = TwoListDeque.peek_first x);;
+assert("bye" = TwoListDeque.peek_first ah_hi_bye_check);;
 
-
-open deque;; 
 let q1 = TwoListDeque.empty;;
 assert(0=(TwoListDeque.size q1));;
+
+
 
 (***
 Added a test case to test the add_first function of TwoListDeque
 ***)
 let q2 = TwoListDeque.add_first 1 q1;;
-assert([1] = TwoListDeque.remove_first q2);;
+assert(TwoListDeque.empty = TwoListDeque.remove_first q2);;
 
 (***
 Added a test case to test the add_last function of TwoListDeque
 ***)
 let q3 = TwoListDeque.add_last 1 q2;;
-assert([1] = TwoListDeque.remove_last q3);;
+assert([1] = TwoListDeque.to_list (TwoListDeque.remove_last q3));;
 (***
 add_first test case
 ***)
-let q4 = TwoListDeque.add_first [1;2;3] q3;;
+let q4 = TwoListDeque.add_first 1 q3;;
 assert(1 = TwoListDeque.peek_first q4);; 
-assert(3 = TwoListDeque.peek_last q4);;
+assert(1 = TwoListDeque.peek_last q4);;
+assert([1;1;1] = TwoListDeque.to_list q4);;
+
 (***
 add_last test case
 ***)
-let q5 = TwoListDeque.add_last [4;5;6] q3;;
-assert(6 = TwoListDeque.size q5);;
+let q5 = TwoListDeque.add_last 6 q3;;
 assert(6 = TwoListDeque.peek_last q5);;
-(***
+assert([1;1;6] = TwoListDeque.to_list q5);;
+
+(* **
 remove_first test case
 ***)
 let q6 = TwoListDeque.remove_first q5;;
-assert([2;3;4;5;6] = TwoListDeque.remove_first q6);;
+assert([1;6] = TwoListDeque.to_list  q6);;
+assert([6] = TwoListDeque.to_list (TwoListDeque.remove_first q6));;
 
 (***
 map test case
 ***)
-let q7 = TwoLIstDeque.map (fun x -> x+1) q6;;
-assert([3] = TwoListDeque.remove_first q7);;
+let q7 = TwoListDeque.map (fun x -> x+1) q6;;
+assert(2 = TwoListDeque.peek_first q7);;
 
 let q10 = TwoListDeque.map (fun x -> x+1) q7;;
-assert([4] = TwoListDeque.remove_first q10);;
+assert(3 = TwoListDeque.peek_first q10);;
 
 (*** 
 filter test cases
 ***)
 let q8 = TwoListDeque.filter (fun x -> x mod 2 = 0) q6;;
-assert([2;4;6] = TwoListDeque.remove_first q8);;
+assert([6] = TwoListDeque.to_list q8);;
 
 let q9 = TwoListDeque.filter (fun x -> x mod 2 = 1) q6;;
-assert([1;3;5] = TwoListDeque.remove_first q9);;
+assert([1] = TwoListDeque.to_list q9);;
 
 
 (***
@@ -100,15 +106,14 @@ assert(100 = TwoListDeque.peek_last q13);;
 (*jonahs tests start*)
 (* testing size 2 *)
 let jqueue = TwoListDeque.empty;;
-TwoListDeque.add_first "no" jqueue;;
-TwoListDeque.add_last "hi" jqueue;;
-assert(2 = (TwoListDeque.size jqueue));;
-assert("hi" = (TwoListDeque.peek_last jqueue))
-assert("no" = (TwoListDeque.peek_first jqueue))
-TwoListDeque.remove_first jqueue
-assert(1 = (TwoListDeque.size jqueue));;
-assert("hi" = (TwoListDeque.peek_last jqueue))
-assert("hi" = (TwoListDeque.peek_first jqueue))
+let j1 = TwoListDeque.add_first "no" jqueue;;
+let j2 = TwoListDeque.add_last "hi" j1;;
+assert(2 = (TwoListDeque.size j2));;
+assert("hi" = (TwoListDeque.peek_last j2));;
+let j3 = TwoListDeque.remove_first j2;;
+assert(1 = (TwoListDeque.size j3));;
+assert("hi" = (TwoListDeque.peek_last j3));;
+assert("hi" = (TwoListDeque.peek_first j3));;
 (* jonahs test end*)
 
 
@@ -119,16 +124,16 @@ assert((TwoListDeque.peek_first test_add_first)=3);;
 let test_add_last = TwoListDeque.add_last 4 test_add_first;;
 assert((TwoListDeque.peek_first test_add_last)=3);;
 assert((TwoListDeque.peek_last test_add_last)=4);;
-assert((TwoListDeque.to_list test_add_last)=[3;4])
+assert((TwoListDeque.to_list test_add_last)=[3;4]);;
 
 let test_remove_first = TwoListDeque.remove_first test_add_last;;
-assert((TwoListDeque.peek_first)=4);;
+assert((TwoListDeque.peek_first test_remove_first) = 4);;
 
 let test_remove_last = TwoListDeque.remove_last test_remove_first;;
-assert((TwoListDeque.is_empty test_remove_last)=true)
+assert((TwoListDeque.is_empty test_remove_last)=true);;
 
 
-assert(TwoListDeque.is_empty q1);;
+assert(TwoListDeque.is_empty q1 = true);;
 
 
 
